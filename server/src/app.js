f01e13ccb02729e1e17f75557b6b879b7911f4b5
Teplov <vladimir.teplov@gmail.com>
2020-08-13 16:34:21 +0300

    add winston logger

--- server/src/app.js
+++ server/src/app.js
@@ -4,9 +4,24 @@ const verbose = process.env.NODE_ENV !== 'test';
 const escapeHtml = require('escape-html');
 const bodyParser = require('body-parser');
 const morgan = require('morgan')
+const log = require('./libs/logger')(module);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('tiny'))
+app.use(function (req, res, next) {
+    res.status(404);
+    log.debug('Not found URL: %s', req.url);
+    res.send({error: 'Not found'});
+    return;
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    log.error('Internal error(%d): %s', res.statusCode, err.message);
+    res.send({error: err.message});
+    return;
+});
+
 
 require('dotenv').config();
 require('./routes/utils')(app, verbose);
@@ -15,7 +30,7 @@ require('./routes/users')(app);
 
 const port = process.env.SERVER_PORT;
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    log.info(`Example app listening at http://localhost:${port}`)
 });
 
 
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -7,14 +7,25 @@ module.exports = function(app) {
 
     app.post('/api/v1/users', function(req, res) {
         let user = new User({
-            username: "Test"
+            username: req.body.username,
+            password: req.body.password
         });
-        user.set('password','test');
         user.save(function(err, user, affected){
-            if (err) throw err;
-            res.send('User created!');
+            if (!err) {
+                log.info("user created");
+                return res.send({ status: 'OK', user:user });
+            } else {
+                console.log(err);
+                if(err.name == 'ValidationError') {
+                    res.statusCode = 400;
+                    res.send({ error: 'Validation error' });
+                } else {
+                    res.statusCode = 500;
+                    res.send({ error: 'Server error' });
+                }
+                log.error('Internal error(%d): %s',res.statusCode,err.message);
+            }
         });
-        res.send('...');
     });
 
 
