3cb55ab97ec843634a93f757c12c970c50685fd5
Teplov <vladimir.teplov@gmail.com>
2020-08-13 17:35:38 +0300

    fix logger

--- server/src/app.js
+++ server/src/app.js
@@ -8,20 +8,6 @@ const log = require('./libs/logger')(module);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('tiny'))
-app.use(function (req, res, next) {
-    res.status(404);
-    log.debug('Not found URL: %s', req.url);
-    res.send({error: 'Not found'});
-    return;
-});
-
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    log.error('Internal error(%d): %s', res.statusCode, err.message);
-    res.send({error: err.message});
-    return;
-});
-
 
 require('dotenv').config();
 require('./routes/utils')(app, verbose);
