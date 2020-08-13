202fc78abbd92859e60ea64c16c82f806418cc87
Teplov <vladimir.teplov@gmail.com>
2020-08-13 16:36:24 +0300

    log db connection errors

--- server/src/libs/mongoose.js
+++ server/src/libs/mongoose.js
@@ -1,6 +1,15 @@
 let mongoose = require('mongoose');
 let config = require('./../config/db');
+let log = require('logger');
 
 mongoose.connect(process.env.MONGODB_CONN_STRING, config.mongoose.options);
+let db = mongoose.connection;
+
+db.on('error', function (err) {
+    log.error('connection error:', err.message);
+});
+db.once('open', function callback () {
+    log.info("Connected to DB!");
+});
 
 module.exports = mongoose;
