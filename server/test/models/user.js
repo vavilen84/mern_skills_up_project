49d0cb6df8bd89111788ca8c284d068e62542f02
Teplov <vladimir.teplov@gmail.com>
2020-08-14 21:04:19 +0300

    add user tests

--- server/test/models/user.js
+++ server/test/models/user.js
@@ -8,7 +8,7 @@ describe('User model validation', function () {
 
     describe('scenario create', function () {
 
-        describe('password', function () {
+        describe('validate password on create', function () {
             describe('required password validation', function () {
                 it('error on empty password', function () {
                     let user = new User({});
@@ -35,7 +35,7 @@ describe('User model validation', function () {
             });
         });
 
-        describe('username', function () {
+        describe('validate username on create', function () {
             describe('required username validation', function () {
                 it('error on empty username', function () {
                     let user = new User({});
@@ -57,29 +57,37 @@ describe('User model validation', function () {
 
     describe('scenario update', function () {
 
-        describe('password', function () {
-            describe('required password validation', function () {
-                it('error on empty password', function () {
+        describe('validate password on update', function () {
+            describe('password should not be set and validated on update', function () {
+                it('set password on update', function () {
                     let user = new User({});
-                    user.set('scenario', enums.Models.SCENARIO_CREATE);
+                    user.set('scenario', enums.Models.SCENARIO_UPDATE);
+                    user.set('password', 'string');
+                    assert.strictEqual(!user.get('password'), true)
+                });
+                it('skip validate password on update', function () {
+                    let user = new User({});
+                    user.set('scenario', enums.Models.SCENARIO_UPDATE);
                     let errors = user.validateSync();
-                    assert.notStrictEqual(errors.errors['hashedPassword'].message, true);
+                    assert.notStrictEqual(errors.errors['hashPassword'], false);
+                });
+            });
+        });
+
+        describe('validate username on update', function () {
+            describe('required username validation', function () {
+                it('error on empty username', function () {
+                    let user = new User({});
+                    user.set('scenario', enums.Models.SCENARIO_UPDATE);
+                    let errors = user.validateSync();
+                    assert.notStrictEqual(errors.errors['username'].message, true);
                 });
                 it('no error', function () {
                     let user = new User({});
-                    user.set('scenario', enums.Models.SCENARIO_CREATE);
-                    user.set('password', 'string');
+                    user.set('scenario', enums.Models.SCENARIO_UPDATE);
+                    user.set('username', 'username');
                     let errors = user.validateSync();
-                    assert.notStrictEqual(errors.errors['hashedPassword'], false);
-                });
-            });
-            describe('hash password', function () {
-                it('password is hashed', function () {
-                    let user = new User({});
-                    user.set('scenario', enums.Models.SCENARIO_CREATE);
-                    const plainPassword = 'string';
-                    user.set('password', plainPassword);
-                    assert.strictEqual(plainPassword !== user.get('hashedPassword'), true);
+                    assert.notStrictEqual(errors.errors['username'], false);
                 });
             });
         });
