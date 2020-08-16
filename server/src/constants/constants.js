8489dd26c155e892157eea286afc21ba6c5e21fd
Teplov <vladimir.teplov@gmail.com>
2020-08-16 14:12:14 +0300

    add constants

--- server/src/constants/constants.js
+++ server/src/constants/constants.js
@@ -1,1 +1,21 @@
 module.exports.MONGOOSE_VALIDATION_ERR_KEY = 'ValidationError';
+
+module.exports.USERS_COLLECTION_NAME = 'Users';
+
+module.exports.USERS_BASE_URL = '/api/v1/users';
+
+module.exports.RESPONSE_CODE = {
+    UNPROCESSABLE_ENTITY: 422,
+    OK: 200,
+    CREATED: 201,
+    VALIDATION_ERROR: 400,
+    SERVER_ERROR: 500
+};
+
+module.exports.RESPONSE_MESSAGE = {
+    UNPROCESSABLE_ENTITY: 'Unprocessable entity',
+    OK: 'OK',
+    CREATED: 'Created',
+    VALIDATION_ERROR: 'Validation error',
+    SERVER_ERROR: 'Server error'
+};
