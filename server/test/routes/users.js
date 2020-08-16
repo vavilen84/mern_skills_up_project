3854769939bc7101a546b6f666787a37180f7fcd
Teplov <vladimir.teplov@gmail.com>
2020-08-16 14:13:47 +0300

    tests

--- server/test/routes/users.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-describe('/api/v1/users', function () {
-    describe('POST', function () {
-        it('get validation error - empty body', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
-    });
-});
