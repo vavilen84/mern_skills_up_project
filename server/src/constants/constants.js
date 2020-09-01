5f5318eff7718d889df9cdede5af585393feeae2
Teplov <vladimir.teplov@gmail.com>
2020-09-01 12:55:19 +0300

    add constants

--- server/src/constants/constants.js
+++ server/src/constants/constants.js
@@ -7,7 +7,8 @@ module.exports.USERS_BASE_URL = '/api/v1/users';
 module.exports.RESPONSE_CODE = {
     UNPROCESSABLE_ENTITY: 422,
     OK: 200,
-    NOT_FOUND: 400,
+    NOT_FOUND: 404,
+    NOT_UNAUTHORIZED: 401,
     CREATED: 201,
     VALIDATION_ERROR: 400,
     SERVER_ERROR: 500
@@ -17,6 +18,7 @@ module.exports.RESPONSE_MESSAGE = {
     UNPROCESSABLE_ENTITY: 'Unprocessable entity',
     OK: 'OK',
     NOT_FOUND: 'Not found',
+    NOT_UNAUTHORIZED: 'Unauthorized',
     CREATED: 'Created',
     VALIDATION_ERROR: 'Validation error',
     SERVER_ERROR: 'Server error'
