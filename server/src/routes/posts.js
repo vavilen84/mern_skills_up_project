c8d51b390c81dd0082fe72ac4520c55e0714d3bc
Teplov <vladimir.teplov@gmail.com>
2020-09-01 13:29:30 +0300

    refactoring

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -6,34 +6,4 @@ module.exports = function(app) {
         response.sendOK(res, {"data":1}, "message");
     });
 
-
-    // let posts = {
-    //     list: function (req, res) {
-    //         res.send('user list');
-    //     },
-    //
-    //     get: function (req, res) {
-    //         res.send('user ' + escapeHtml(req.params.uid))
-    //     },
-    //
-    //
-    //     post: function (req, res) {
-    //         res.send('user ' + escapeHtml(req.params.uid))
-    //     },
-    //
-    //     delete: function (req, res) {
-    //         res.send('delete users');
-    //     }
-    // };
-    //
-    // app.map({
-    //     '/posts': {
-    //         get: posts.list,
-    //         '/:uid': {
-    //             get: posts.get,
-    //             post: posts.post,
-    //             delete: posts.delete,
-    //         },
-    //     }
-    // });
 };
