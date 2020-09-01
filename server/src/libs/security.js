cfb4ec9cb431b27e925e6495837d8b711001e065
Teplov <vladimir.teplov@gmail.com>
2020-09-01 12:56:55 +0300

    security: add generate tokens

--- server/src/libs/security.js
+++ server/src/libs/security.js
@@ -6,5 +6,22 @@ exports.checkPassword = function (password, salt, hashedPassword) {
 }
 
 exports.encryptPassword = function (password, salt) {
-    return security.createHmac('sha1', salt+secret).update(password).digest('hex');
+    return security.createHmac('sha1', salt + secret).update(password).digest('hex');
+}
+
+exports.generateAuthTokens = function () {
+    return {
+        accessToken: generateHash(),
+        refreshToken: generateHash()
+    };
+}
+
+function generateHash() {
+    let hash = 0, i, chr;
+    for (i = 0; i < this.length; i++) {
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
 }
