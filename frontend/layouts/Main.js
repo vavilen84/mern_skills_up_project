2800af184119da15e17d6a84a0f2942743d8bb63
Teplov <vladimir.teplov@gmail.com>
2020-09-04 12:50:41 +0300

    frontend: fix top nav styles

--- frontend/layouts/Main.js
+++ frontend/layouts/Main.js
@@ -2,21 +2,19 @@ import Head from 'next/head'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 
-export default ({ children, title = 'This is the default title' }) => (
-  <div>
-    <Head>
-      <title>{ title }</title>
-    </Head>
-    <header>
-      <Nav />
-    </header>
+export default ({children, title = 'This is the default title'}) => (
+    <div>
+        <Head>
+            <title>{title}</title>
+        </Head>
+        <Nav/>
 
-    <main>
-      { children }
-    </main>
+        <div className="container">
+            {children}
+        </div>
 
-    <Footer>
-      Footer
-    </Footer>
-  </div>
+        <Footer>
+            Footer
+        </Footer>
+    </div>
 )
