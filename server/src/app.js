48265155eb861a9cfd04a231b2e1a0f0ab69145f
Teplov <vladimir.teplov@gmail.com>
2020-11-09 18:59:35 +0200

    server: add cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,20 @@
 const log = require('./libs/logger')(module);
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const verbose = process.env.NODE_ENV !== 'test';
 //const escapeHtml = require('escape-html');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 //const morgan = require('morgan')
 //app.use(morgan('tiny'))
+app.use(cors());
 
 require('dotenv').config();
 if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test' });
 }
 if (process.env.NODE_ENV === 'local') {
-    console.log(123);
     require('dotenv').config({ path: '.env.local' });
 }
 require('./routes/utils')(app, verbose);
