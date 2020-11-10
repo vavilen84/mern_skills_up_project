9d2f29a232d7aad143ca171082a6102272d57024
Teplov <vladimir.teplov@gmail.com>
2020-11-10 14:21:30 +0200

    docker env

--- server/src/app.js
+++ server/src/app.js
@@ -3,19 +3,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const verbose = process.env.NODE_ENV !== 'test';
-//const escapeHtml = require('escape-html');
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
-//const morgan = require('morgan')
-//app.use(morgan('tiny'))
 app.use(cors());
 
 require('dotenv').config();
 if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test' });
-}
-if (process.env.NODE_ENV === 'local') {
-    require('dotenv').config({ path: '.env.local' });
 }
 require('./routes/utils')(app, verbose);
 require('./routes/posts')(app);
