dd93c4448c452253e49581141ac5c228c0af67f7
Teplov <vladimir.teplov@gmail.com>
2020-11-26 13:45:09 +0200

    temp env hacks

--- frontend/src/helpers/index.js
+++ frontend/src/helpers/index.js
@@ -1,6 +1,8 @@
 export const USERS_BASE_URL = '/api/v1/users';
 
-export const SERVER_URL = 'http://server.react_node_blog_local:8000'
+// TODO
+//export const SERVER_URL = 'http://server.react_node_blog_local:8000'
+export const SERVER_URL = 'http://localhost:8000'
 
 export const getURL = (url) => (
     SERVER_URL+url
