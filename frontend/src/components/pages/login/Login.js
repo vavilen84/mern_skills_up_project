7c34ebe006a8d18ba59d4987a6ba8cac49ca7f34
Teplov <vladimir.teplov@gmail.com>
2020-12-11 19:25:25 +0200

    frontend: login

--- frontend/src/components/pages/login/Login.js
+++ frontend/src/components/pages/login/Login.js
@@ -1,11 +1,26 @@
 import React from "react";
 import LoginForm from "./LoginForm";
+import {connect} from "react-redux";
+import {Redirect} from "react-router-dom";
 
-const Login = () => (
-    <div>
-        <h1>Login</h1>
-        <LoginForm/>
-    </div>
-)
+const Login = (props) => {
+    if (props.isLoggedIn) {
+        return <Redirect to={'/'}/>;
+    }
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm/>
+        </div>
+    )
+}
 
-export default Login;
+const mapStateToProps = (state) => {
+    let auth = state.rootReducer.auth;
+
+    return {
+        isLoggedIn: auth.accessToken !== null,
+    };
+}
+
+export default connect(mapStateToProps, null)(Login);
