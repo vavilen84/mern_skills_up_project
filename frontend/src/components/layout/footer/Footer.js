ca98c098b9d29cc0796b8250d051e791d3df3035
vavilen84 <vladimir.teplov@gmail.com>
2021-12-24 17:56:05 +0200

    refactoring

--- frontend/src/components/layout/footer/Footer.js
+++ frontend/src/components/layout/footer/Footer.js
@@ -1,24 +1,13 @@
 import React from "react";
 import Holder from "../holder/Holder";
 import "./style.scss";
-import LinkedinIcon from '../../../static/img/icons/linkedin.svg'
-import GithubIcon from '../../../static/img/github.png'
 
 function Footer() {
+    let date = new Date().getFullYear();
     return (
         <div className={'footer'}>
             <Holder>
-                <a className={'icon'} target={'_blank'} href={'https://www.linkedin.com/in/vladimir-teplov-5b8b936a/'}>
-                    <img src={LinkedinIcon} alt={''}/>
-                </a>
-                <a className={'icon'} target={'_blank'} href={'https://github.com/vavilen84'}>
-                    <img src={GithubIcon} alt={''}/>
-                </a>
-                <div className={'fl-left text-block'}>
-                    <div className={'text'}>E-mail: vladimir.teplov@gmail.com</div>
-                    <div className={'text'}>Skype: vavilen1984</div>
-                </div>
-                <div className={'clear'}/>
+                <div className={'footer-info'}>@{date} vavilen84</div>
             </Holder>
         </div>
     );
--- frontend/src/components/pages/frontend/home/Home.js
+++ frontend/src/components/pages/frontend/home/Home.js
@@ -1,17 +1,9 @@
 import React from "react";
-import Me from '../../../../static/img/me.jpeg'
 import './style.scss'
 
 const Home = () => (
     <div className={'index-page'}>
-        <div className={'fl-left me col-6 col-lg-4'}>
-            <img src={Me} alt={''}/>
-        </div>
-        <div className={'fl-left pl20p greeting col-sm-6 col-lg-8'}>
-            Hi, my name is Vladimir, I am a software engineer. This is my personal website and I am glad
-            to see you here. Here you can find some words about me & my experience.
-        </div>
-        <div className={'clear'}/>
+
     </div>
 );
 
