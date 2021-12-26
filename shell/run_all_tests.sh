60febfffd229ad5c2daa3527351b5b2899d55f91
vavilen84 <vladimir.teplov@gmail.com>
2021-12-26 15:33:55 +0200

    shell: fix passing env

--- shell/run_all_tests.sh
+++ shell/run_all_tests.sh
@@ -1,3 +1,3 @@
-APP_ENV=test server/./node_modules/mocha/bin/mocha server/src/test/models/*.test.js --exit
-APP_ENV=test server/./node_modules/mocha/bin/mocha server/src/test/endpoints/**/*.test.js --exit
+NODE_ENV=test server/./node_modules/mocha/bin/mocha server/src/test/models/*.test.js --exit
+NODE_ENV=test server/./node_modules/mocha/bin/mocha server/src/test/endpoints/**/*.test.js --exit
 
